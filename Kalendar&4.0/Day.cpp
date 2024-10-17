5971e5fca71b096aae4107bbdb1d5452ca8fb682
Koniakk <158475062+Koniakk@users.noreply.github.com>
2024-10-17 19:48:44 +0300

    Update Day.cpp

--- Kalendar&4.0/Day.cpp
+++ Kalendar&4.0/Day.cpp
@@ -1,28 +1,26 @@
-int id;
-	int momentDate; //chislo
-	string mark; //zametki
+#include "Day.h"
 
-	Day(int id, int momentDate, const string& mark)
-		: id(id), momentDate(momentDate), mark(mark) {}
+Day::Day(int id, int momentDate, const string& mark)
+	: id(id), momentDate(momentDate), mark(mark) {}
 
 	// Конструктор копирования
-	Day(int id, const Day& other)
-		: id(id), momentDate(momentDate), mark(other.mark) {}
+Day::Day(int id, const Day& other)
+	: id(id), momentDate(momentDate), mark(other.mark) {}
 
-	json toJson() const {
-		return json{
-			{"id",id},
-			{"momentDate", momentDate},
-			{"", mark}
-		};
-	}
+json Task:: toJson() const {
+	return json{
+		{"id",id},
+		{"momentDate", momentDate},
+		{"", mark}
+	};
+}
 
-	static Day fromJson(const json& j) {
-		return Day(j["id"], j["momentDate"], j["mark"]);
-	}
+Day Day:: fromJson(const json& j) {
+	return Day(j["id"], j["momentDate"], j["mark"]);
+}
 
-	string ToString() {
-		return to_string(id)
-			+ " " + to_string(momentDate)         
-			+ " " + mark;
-	}
+Day::string ToString() {
+	return to_string(id)
+		+ " " + to_string(momentDate)         
+		+ " " + mark;
+}
